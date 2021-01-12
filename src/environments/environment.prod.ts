6f98cd5b6242b6eb9df1b6e450024e56b2440de7
SeanBaraka <seanbaraka@gmail.com>
2021-01-12 08:36:13 +0300

    start of periodic alerts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://192.168.0.105:3010/',
-  recipients: ['+254727275739']
+  recipients: ['+254727275739'],
+  socketUrl: 'http://192.168.0.105:3010',
   // apiBaseUrl: 'http://localhost:3000' and some other values
 };
